616a41fb1e82aab48bbfd98bb2de7d0e941576bf
DavidNinMur <dninmur@gmail.com>
2023-03-31 21:26:52 +0200

    section added

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div class="wrapper">
       <AppTopBar></AppTopBar>
       <AppVisualLayout></AppVisualLayout>
+      <AppSectionTittle></AppSectionTittle>
     </div>
   </header>
 
@@ -15,12 +16,14 @@ import { RouterLink, RouterView } from 'vue-router'
 
 import AppTopBar from "./components/top-bar/AppTopBar.vue"
 import AppVisualLayout from "./components/generics/visual-layout/AppVisualLayout.vue"
+import AppSectionTittle from "./components/generics/section-tittle/AppSectionTittle.vue"
 
 export default {
   name: "App",
   components:{
     AppTopBar,
-    AppVisualLayout
+    AppVisualLayout,
+    AppSectionTittle
   }, 
   setup(){
 
---
+++ src/components/generics/section-tittle/AppSectionTittle.vue
@@ -0,0 +1,9 @@
+<template src="./AppSectionTittle.html"></template>
+
+<script>
+export default {
+    name: 'AppSectionTittle'
+}
+</script>
+
+<style lang="scss" src="./AppSectionTittle.scss"></style>
