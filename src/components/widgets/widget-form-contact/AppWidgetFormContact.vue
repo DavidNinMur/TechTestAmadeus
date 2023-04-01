97045a490f5aed4150a41c2a8e40fac413412075
DavidNinMur <dninmur@gmail.com>
2023-04-01 18:21:25 +0200

    form validation done

--- src/components/widgets/widget-form-contact/AppWidgetFormContact.vue
+++ src/components/widgets/widget-form-contact/AppWidgetFormContact.vue
@@ -1,8 +1,68 @@
 <template src="./AppWidgetFormContact.html"></template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-    name: 'AppWidgetFormContact'
+    name: 'AppWidgetFormContact',
+    props:{},
+    emits:[''],
+    setup(props, { emit }) {
+
+        const firtsNameRefStr = ref('');
+        const lastNameRefStr = ref('');
+        const emailRefStr = ref('');
+        const telephoneNumberRefInt = ref();
+        const messageRefStr = ref('');
+        const selectedTermsAndConditionsRefBool = ref(false);
+        
+        const isInvalidEmailRefBool = ref(false);
+        const submittedFormRefBool = ref(false);
+
+        const setValidEmail = () => {
+            const regexToValidateEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return regexToValidateEmail.test(emailRefStr.value)
+        }
+
+        const onClickTermsButton = () => {
+            selectedTermsAndConditionsRefBool.value = !selectedTermsAndConditionsRefBool.value;
+        }
+
+        const onSubmitForm = () => {
+            submittedFormRefBool.value = true;
+            isInvalidEmailRefBool.value = false;
+            if (!firtsNameRefStr.value || !lastNameRefStr.value) {
+                alert('Por favor ingresa tu nombre y apellido')
+                return
+            }
+            if(!emailRefStr.value){
+                alert('Por favor ingresa tu email')
+                return
+            }
+            if(!setValidEmail()){
+                isInvalidEmailRefBool.value = true;
+                alert('Por favor ingresa un email valido')
+                return
+            }
+
+            console.log(`Nombre: ${firtsNameRefStr.value}, Apellido: ${lastNameRefStr.value}, Email: ${emailRefStr.value}, Telephone: ${telephoneNumberRefInt.value}, Message: ${messageRefStr.value}, TermsAccepted: ${selectedTermsAndConditionsRefBool.value}`)
+        } 
+
+        return {
+            firtsNameRefStr,
+            lastNameRefStr,
+            emailRefStr,
+            telephoneNumberRefInt,
+            messageRefStr,
+            selectedTermsAndConditionsRefBool,
+            
+            isInvalidEmailRefBool,
+            submittedFormRefBool,
+
+            onClickTermsButton,
+            onSubmitForm
+        }
+    }
 }
 </script>
 
